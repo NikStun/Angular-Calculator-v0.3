d84b9fb72d11ecb1281efa708d4b522be0dc6087
Nik <nikist99@gmail.com>
2019-01-02 09:34:49 +0400

    v 0.4 Added styles

--- src/app/calc/calc.module.ts
+++ src/app/calc/calc.module.ts
@@ -4,7 +4,6 @@ import { CalcComponent } from './calc/calc.component';
 import { OutputWindowComponent } from './output-window/output-window.component';
 import { ButtonPadComponent } from './button-pad/button-pad.component';
 import { CalcButtonComponent } from './calc-button/calc-button.component';
-import { CalcHisComponent } from './calc-his/calc-his.component';
 
 
 @NgModule({
