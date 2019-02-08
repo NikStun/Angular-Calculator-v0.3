84ad4fd8edd9e26c6b0343f35d61532dd67ff4a6
Nik <nikist99@gmail.com>
2019-02-08 19:03:00 +0400

    Added query PUT, that save all info about credits and payments to DB

--- src/app/credit-calc/credit-calc.component.ts
+++ src/app/credit-calc/credit-calc.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IDatePickerConfig } from 'ng2-date-picker';
 import {DatePickerDirective} from 'ng2-date-picker';
-import { CreditService } from '../services/credit.service';
+import { CreditService, BankData } from '../services/credit.service';
 import { CreditData } from '../services/credit.service';
 
 
@@ -23,13 +23,17 @@ export class CreditCalcComponent implements OnInit {
   };
 
   creditMas: CreditData[];
+  creditRes: Response;
+  done: boolean = false;
+  bankMas: BankData[];
 
   constructor(private _creditService: CreditService) {
     this.creditForm = new FormGroup({
       amountOfCredit: new FormControl(0, [Validators.required, Validators.min(0)]),
       timeOfCredit: new FormControl(1, [Validators.required, Validators.min(1), Validators.max(360)]),
       percentOfCredit: new FormControl(10, [Validators.required, Validators.min(0), Validators.max(100), Validators.pattern('^[-+]?[0-9]*[.,]?[0-9]+(?:[eE][-+]?[0-9]+)?$')]),
-      startingDate: new FormControl(null, Validators.required)
+      startingDate: new FormControl(null, Validators.required),
+      bankId: new FormControl(Validators.required)
     });
    }
 
@@ -51,7 +55,25 @@ export class CreditCalcComponent implements OnInit {
   }
 
 
+
+
+  putAll(creditForm: FormGroup){
+  let dateOfCredit = this.creditForm.controls['startingDate'].value.split('.');
+  let day = dateOfCredit[0];
+  let month = dateOfCredit[1];
+  let year = dateOfCredit[2];
+  let modifiedDate = year + '-' + month + '-' + day;
+    this._creditService.putCredit(this.creditForm.controls['bankId'].value, this.creditForm.controls['amountOfCredit'].value, this.creditForm.controls['timeOfCredit'].value, this.creditForm.controls['percentOfCredit'].value, modifiedDate, this.creditMas)
+    .subscribe((data: Response)=>{this.creditRes = data; this.done=true;}),
+     error => console.log(error);
+  }
+
+  getBanks(){
+    this._creditService.getBank().subscribe((result: BankData[])=>{this.bankMas = result;})
+  }
+
   ngOnInit() {
+    this.getBanks();
   }
 
 }
--- src/app/services/credit.service.ts
+++ src/app/services/credit.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class CreditService {
 
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   calculateCredit(amountCredit: number, timeCredit: number, percentCredit: number, startingDate: Date ){
     let pay = Math.round(100*(amountCredit * (percentCredit / 1200 + (percentCredit / 1200) / ((Math.pow((1 + percentCredit / 1200),timeCredit)) - 1))))/100;
@@ -28,9 +28,8 @@ export class CreditService {
       mainDebt = pay - amountOfPercent;
       debtOfCredit = 0;
     }
-
     previousDate = new Date(dateOfPay);
-    creditMas.push(<CreditData>{numOfPay: numOfPay, pay: pay, amountOfPercent: amountOfPercent, mainDebt: mainDebt, debtOfCredit: debtOfCredit, dateOfPay: dateOfPay});
+    creditMas.push(<CreditData>{numOfPay: numOfPay, pay: pay, mainDebt: mainDebt, amountOfPercent: amountOfPercent, debtOfCredit: debtOfCredit, dateOfPay: dateOfPay});
     }
     return creditMas;
 
@@ -52,6 +51,22 @@ export class CreditService {
     return dateOfNextMonth;
   }
 
+  putCredit(bankId: number, amountCredit: number, timeCredit: number, percentCredit: number, startingDate: string, creditMas: CreditData[]){
+    let creditAndPayments = {id_bank: bankId, amount: amountCredit, period: timeCredit, percent: percentCredit, date: startingDate, payments: creditMas};
+    console.log(creditAndPayments);
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.set('Accept', 'application/json');
+    headers = headers.set('Content-Type', 'application/json');
+    let options = {
+            headers: headers
+        };
+    return this._http.put('http://localhost:3000/api/credit', JSON.stringify(creditAndPayments), options);
+
+  }
+  getBank(){
+    return this._http.get('http://localhost:3000/api/banks');
+  }
+
 
 }
 export class CreditData{
@@ -61,4 +76,14 @@ export class CreditData{
   public mainDebt: number;
   public debtOfCredit: number;
   public dateOfPay: Date;
+}
+
+export class Response{
+  public status: number;
+  public message: string;
+}
+
+export class BankData{
+  bankId: number;
+  bankName: string;
 }
